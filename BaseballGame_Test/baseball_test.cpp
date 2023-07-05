53bf65b3456d5c40f7fbee63e1bb3e1968e01f9a
jungi.lee <dffbnm@gmail.com>
2023-07-06 08:57:48 +0900

    [feature] page4
    Signed-off-by: jungi.lee <dffbnm@gmail.com>

--- BaseballGame_Test/baseball_test.cpp
+++ BaseballGame_Test/baseball_test.cpp
@@ -4,7 +4,7 @@
 class BaseballFixture: public testing::Test
 {
 public:
-	Baseball game;
+	Baseball game{ "123" };
 	void assertIllegalArgument(string guessNumber)
 	{
 		try
@@ -23,4 +23,12 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvaidCase) {
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
 	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
