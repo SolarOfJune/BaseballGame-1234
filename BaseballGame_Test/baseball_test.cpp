ea1835f2596dc0d8d0ed79e2d92c2f08cb89d2a8
jungi.lee <dffbnm@gmail.com>
2023-07-05 17:02:28 +0900

    [feature] page2
    Signed-off-by: jungi.lee <dffbnm@gmail.com>

--- BaseballGame_Test/baseball_test.cpp
+++ BaseballGame_Test/baseball_test.cpp
@@ -5,3 +5,8 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
 	baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
