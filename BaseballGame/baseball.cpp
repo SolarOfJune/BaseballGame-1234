e8921ce0b9260d67821309d6cdaf365a4782dce3
jungi.lee <dffbnm@gmail.com>
2023-07-05 16:59:21 +0900

    [feature] page1
    Signed-off-by: jungi.lee <dffbnm@gmail.com>

--- BaseballGame/baseball.cpp
+++ BaseballGame/baseball.cpp
@@ -1,5 +1,11 @@
+#include <stdexcept>
+using namespace std;
+
 class baseball
 {
 public:
-
+	void guess(const string& string)
+	{
+		throw length_error("Must be three letters.");
+	}
 };
--- BaseballGame_Test/baseball_test.cpp
+++ BaseballGame_Test/baseball_test.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "../BaseballGame/baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
